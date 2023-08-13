15244875d4c541a8477e1f495efbc60dc84656d9
Vitto <germanyvitto@gmail.com>
2023-08-13 15:03:17 +0200

    Functional System

--- 02 JsGeneticAlgorithms/07 Steering Vehicles/Rocket.js
+++ 02 JsGeneticAlgorithms/07 Steering Vehicles/Rocket.js
@@ -7,12 +7,28 @@ function Rocket() {
   this.dna = [];
 
   //attraction factor
-  this.dna[0] = random(-3, 3); //food
-  this.dna[1] = random(-3, 3); //poison
+  this.dna[0] = random(0, 3); //food
+  this.dna[1] = random(-3, 0); //poison
 
-  this.behaviour = function () {};
+  this.isDeath = function () {
+    return this.health < 0;
+  };
 
-  //computes the attraction force to the food, calls applyForce
+  //controls, eat(), seek() applyForce()
+  this.behaviour = function (food_list, poison_list) {
+    let foodVector = this.eat(food_list, 1);
+    let poisonVector = this.eat(poison_list, -0.2);
+
+    //do I feel atracted? or repelled?
+    foodVector.mult(this.dna[0]);
+    poisonVector.mult(this.dna[1]);
+
+    //applyForce
+    this.applyForce(foodVector);
+    this.applyForce(poisonVector);
+  };
+
+  //computes the attraction force to the food, returns Force
   this.seek = function (target) {
     // distance bwn the two points
     let desired = p5.Vector.sub(target, this.pos);
@@ -20,9 +36,10 @@ function Rocket() {
     let steer = p5.Vector.sub(this.vel, desired);
     steer.limit(3);
 
-    return this.applyForce(steer);
+    return steer;
   };
 
+  //finds the closes element, calls seek
   this.eat = function (list, nutrition) {
     let d = Infinity;
     let closest = null; //the closest element is a vector
@@ -35,22 +52,23 @@ function Rocket() {
         closest = list[k];
         d = dn;
       }
-      if (dn < 5) {
+      if (dn < 20) {
+        this.health += nutrition;
         list.splice(k, 1);
       }
     }
     //if found, call seek
     if (closest) {
-      //no food left
-      this.health += nutrition;
+      //returns a vector (look seek)
       return this.seek(closest);
     }
+    //no food left
     return createVector(0, 0);
   };
 
   this.applyForce = function (force) {
     this.acc.sub(force);
-    this.acc.limit(3);
+    //this.acc.limit(5);
   };
 
   this.update = function () {
@@ -61,13 +79,15 @@ function Rocket() {
   };
 
   this.display = function () {
+    //color
+    let col = lerpColor(color(255,0,0), color(0,255,0), this.health);
     // Draw a triangle rotated in the direction of velocity
     let angle = this.vel.heading() + PI / 2;
     push();
     translate(this.pos.x, this.pos.y);
     rotate(angle);
-    fill(255);
-    stroke(255);
+    fill(col,150);
+    stroke(col);
     strokeWeight(1);
     beginShape();
     vertex(0, -this.r * 2);
--- 02 JsGeneticAlgorithms/07 Steering Vehicles/sketch.js
+++ 02 JsGeneticAlgorithms/07 Steering Vehicles/sketch.js
@@ -5,36 +5,57 @@ let poisons = [];
 function setup() {
   createCanvas(400, 400);
   //foods
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 10; i++) {
     foods[i] = createVector(random(width), random(height));
   }
-  /* //poison
-  for (let i = 0; i < 10; i++) {
+  //poison
+  for (let i = 0; i < 20; i++) {
     poisons[i] = createVector(random(width), random(height));
-  } */
+  }
   //vehicles
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 3; i++) {
     rockets[i] = new Rocket();
   }
 }
 
 function draw() {
   background(0);
+
+  if (random() < 0.05) {
+    foods.push(createVector(random(width), random(height)));
+  }
+
+  if (random() < 0.005) {
+    rockets.push(new Rocket());
+  }
+
+  if (random() < 0.01) {
+    poisons.push(createVector(random(width), random(height)));
+  }
+
+  //food
   fill(0, 255, 0);
   for (let i = 0; i < foods.length; i++) {
     ellipse(foods[i].x, foods[i].y, 20, 20);
   }
 
-  /* fill(255, 0, 0);
-
-  for (let i = 0; i < foods.length; i++) {
-    ellipse(poisons[i].x, poisons[i].y, 7, 7);
-  } */
-  for (let i = 0; i < rockets.length; i++) {
-    rockets[i].eat(foods, 0.5);
-    rockets[i].update();
-    rockets[i].display();
+  //poison
+  fill(255, 0, 0);
+  for (let i = 0; i < poisons.length; i++) {
+    ellipse(poisons[i].x, poisons[i].y, 20, 20);
   }
 
-  noLoop();
+  for (let i = rockets.length - 1; i >= 0; i--) {
+    //console.log(i);
+    rockets[i].behaviour(foods, poisons);
+    rockets[i].update();
+    rockets[i].display();
+    rockets[i].health -= 0.01;
+  }
+  for (let i = rockets.length - 1; i >= 0; i--) {
+    if (rockets[i].isDeath()) {
+      rockets.splice(i, 1);
+    }
+  }
+  //noLoop();
 }
