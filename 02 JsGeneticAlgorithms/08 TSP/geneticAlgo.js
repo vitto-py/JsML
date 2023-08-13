4a5ef3d15450b996c2d536af04f2c3329cd16194
Vitto <germanyvitto@gmail.com>
2023-08-14 00:51:17 +0200

    TSP algo genetico functionando

--- 02 JsGeneticAlgorithms/08 TSP/geneticAlgo.js
+++ 02 JsGeneticAlgorithms/08 TSP/geneticAlgo.js
@@ -1,27 +1,73 @@
-function calcFitness(cities, order, index) {
+function arrayFitness() {
+  for (let k = 0; k < population.length; k++) {
+    let fit = calcFitness(population[k], k);
+    fitness[k] = 1 / fit;
+
+    if (fitness[k] > bestDist) {
+      bestDist = fitness[k];
+      bestOrder = population[k].slice(); //best order
+    }
+  }
+}
+
+function calcFitness(order, index) {
   //particular order = [3,2,6,1,...]
   let total = 0;
   for (let i = 0; i < order.length - 1; i++) {
-    let d = dist(cities[order[i]].x, cities[order[i]].y, cities[order[i+1]].x, cities[order[i+1]].y);
+    let d = dist(
+      cities[order[i]].x,
+      cities[order[i]].y,
+      cities[order[i + 1]].x,
+      cities[order[i + 1]].y
+    );
     total += d;
   }
-  fitness[index] = 1/total; //the smaller, the better
+  return total; //the smaller, the better
 }
 
-function fitnessNormalization(fitness) {
+function fitnessNormalization() {
   let total = 0;
-  let bestDist = Infinity;
   for (let ix = 0; ix < fitness.length; ix++) {
     total += fitness[ix];
-    if (fitness[ix] < bestDist) {
-      bestDist = fitness[ix];
-      bestOrder = ix; //at this index
-    }
   }
 
   for (let ix = 0; ix < fitness.length; ix++) {
-    fitnessNorm[i] = fitness[ix]/total;
+    fitnessNorm[ix] = fitness[ix] / total;
   }
+}
+
+function pickOne(listProb) {
+  //listProb = fitnessNorm
+  let arrow = random(1);
+  let index = 0;
+  while (arrow > 0) {
+    arrow = arrow - listProb[index];
+    index++;
+  }
+  index--; //look at the index++, you evaluate buffer in the next cycle
+  return index; //indice de poblacion[index]
+}
+
+
+function nextGeneration() {
+  let newPop = [];
+  for (let j = 0; j < population.length; j++) {
+    ixA = pickOne(fitnessNorm);
+    newPop[j] = population[ixA].slice();
+  }
+
+  for (let j = 0; j < population.length; j++) {
+    mutateOrder(newPop[j], 0.015);
+  }
+
+  population = newPop;
 
 }
 
+function mutateOrder(order, rate) {
+  if (random() < rate) {
+    let ixA = floor(random(order.length));
+    let ixB = floor(random(order.length));
+    orderShuffle(order,ixA, ixB)
+  }
+}
--- 02 JsGeneticAlgorithms/08 TSP/sketch.js
+++ 02 JsGeneticAlgorithms/08 TSP/sketch.js
@@ -1,78 +1,72 @@
 //Visual form of the travel sales person - USES DRAW as main loop
 
-let stops = [];
-let minDist = 0;
+let cities = [];
+let bestDist = 0;
 let bestOrder = [];
 let fitnessNorm = [];
 let fitness = [];
-
+let population = []; //array of array (several orders)
 
 function setup() {
   createCanvas(400, 400);
-  frameRate(1);
-
+  //frameRate(10);
+  //create cities
   for (let i = 0; i < 10; i++) {
-    stops[i] = createVector(random(0.1*width, width*0.9), random(0.1*width, width*0.9));
+    cities[i] = createVector(
+      random(0.1 * width, width * 0.9),
+      random(0.1 * width, width * 0.9)
+    );
   }
 
-  minDist = calcDistance(stops);
-  bestOrder = stops.slice();
+  //order = [0,1,2,3...]
+  let order = [];
+  for (let i = 0; i < cities.length; i++) {
+    order[i] = i;
+  }
 
+  //40 combination of orders
+  for (let i = 0; i < 100; i++) {
+    population[i] = shuffle(order.slice());
+  }
 }
 
 function draw() {
-
-  //draw nodes
   background(0);
+
+  arrayFitness();
+  fitnessNormalization();
+  nextGeneration();
+
+  //draw cities
   fill(255);
-  for (let i = 0; i < stops.length; i++) {
-    ellipse(stops[i].x, stops[i].y,10);
+  noStroke();
+  for (let i = 0; i < cities.length; i++) {
+    ellipse(cities[i].x, cities[i].y, 10);
   }
-
-
-  //Conect dots
-  stroke(255, 180);
-  strokeWeight(2);
-  noFill();
-  beginShape();
-  for (let i = 0; i < stops.length; i++) {
-    vertex(stops[i].x, stops[i].y);
-  }
-  endShape();
-
 
   //draw best path
   //Conect dots
-  stroke(255, 0,255);
+  stroke(255, 0, 255);
   strokeWeight(2);
   noFill();
   beginShape();
-  for (let i = 0; i < stops.length; i++) {
-    vertex(bestOrder[i].x, bestOrder[i].y);
+  for (let i = 0; i < cities.length; i++) {
+    vertex(cities[bestOrder[i]].x, cities[bestOrder[i]].y);
   }
   endShape();
 
-  //shuffle points
-  let i = Math.floor(Math.random()*stops.length);
-  let j = Math.floor(Math.random()*stops.length);
-  orderShuffle(stops,i,j)
-
-  
-
-
-
-
+  //noLoop();
 }
 
+function swap() {
+  //shuffle points
+  let i = Math.floor(Math.random() * cities.length);
+  let j = Math.floor(Math.random() * cities.length);
+  orderShuffle(cities, i, j);
+}
 
-
-
-function orderShuffle(a,i,j) {
+function orderShuffle(a, i, j) {
   let aux = a[i];
   a[i] = a[j];
   a[j] = aux;
-
 }
-
-
-
