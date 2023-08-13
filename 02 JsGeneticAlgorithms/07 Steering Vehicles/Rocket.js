17af27a550e168ad7f5873df54469a78c7cb9aaa
Vitto <germanyvitto@gmail.com>
2023-08-13 23:06:02 +0200

    fully functional

--- 02 JsGeneticAlgorithms/07 Steering Vehicles/Rocket.js
+++ 02 JsGeneticAlgorithms/07 Steering Vehicles/Rocket.js
@@ -1,14 +1,29 @@
-function Rocket() {
-  this.pos = createVector(random(width), random(height));
+function Rocket(position, DNA) {
+  if (position == null) {
+    this.pos = createVector(random(width), random(height));
+  } else {
+    this.pos = position;
+  }
+  if (DNA == null) {
+    this.dna = [];
+
+    //attraction factor
+    this.dna[0] = random(0, 1); //food
+    this.dna[1] = random(-1, 0); //poison
+
+    //perception range
+    this.dna[2] = random(10, 100); //food
+    this.dna[3] = random(10, 100); //poison
+
+    //reproduction range
+    this.dna[4] = random(100, 150); //range
+  } else {
+    this.dna = DNA;
+  }
   this.vel = p5.Vector.random2D(); //default 0,0
   this.acc = createVector(0, 0);
   this.health = 1;
   this.r = 4; //trangle side
-  this.dna = [];
-
-  //attraction factor
-  this.dna[0] = random(0, 3); //food
-  this.dna[1] = random(-3, 0); //poison
 
   this.isDeath = function () {
     return this.health < 0;
@@ -16,8 +31,8 @@ function Rocket() {
 
   //controls, eat(), seek() applyForce()
   this.behaviour = function (food_list, poison_list) {
-    let foodVector = this.eat(food_list, 1);
-    let poisonVector = this.eat(poison_list, -0.2);
+    let foodVector = this.eat(food_list, 1, this.dna[2]);
+    let poisonVector = this.eat(poison_list, -0.2, this.dna[3]);
 
     //do I feel atracted? or repelled?
     foodVector.mult(this.dna[0]);
@@ -32,27 +47,30 @@ function Rocket() {
   this.seek = function (target) {
     // distance bwn the two points
     let desired = p5.Vector.sub(target, this.pos);
+    desired.limit(3);
     // correct direction
-    let steer = p5.Vector.sub(this.vel, desired);
+    let steer = p5.Vector.sub(desired, this.vel);
     steer.limit(3);
 
     return steer;
   };
 
   //finds the closes element, calls seek
-  this.eat = function (list, nutrition) {
+  this.eat = function (list, nutrition, perception) {
     let d = Infinity;
     let closest = null; //the closest element is a vector
 
     //loops through the list, finds the closest, eliminates
     //the food if dn < 5 px
     for (let k = list.length - 1; k >= 0; k--) {
-      let dn = abs(dist(list[k].x, list[k].y, this.pos.x, this.pos.y));
-      if (dn < d) {
+      let dn = this.pos.dist(list[k]);
+      //if it is inside the range he can perceive
+      if (dn < d && dn < perception) {
         closest = list[k];
         d = dn;
       }
-      if (dn < 20) {
+      if (dn < rr) {
+        //If Im close i eat it, even if it is poison
         this.health += nutrition;
         list.splice(k, 1);
       }
@@ -67,26 +85,38 @@ function Rocket() {
   };
 
   this.applyForce = function (force) {
-    this.acc.sub(force);
-    //this.acc.limit(5);
+    this.acc.add(force);
+    this.acc.limit(5);
   };
 
   this.update = function () {
+    this.health -= 0.005;
     this.vel.add(this.acc);
-    this.vel.limit(3);
+    this.vel.limit(5);
     this.pos.add(this.vel);
     this.acc.mult(0);
   };
 
   this.display = function () {
     //color
-    let col = lerpColor(color(255,0,0), color(0,255,0), this.health);
+    let col = lerpColor(color(255, 0, 0), color(0, 255, 0), this.health);
     // Draw a triangle rotated in the direction of velocity
     let angle = this.vel.heading() + PI / 2;
+
     push();
     translate(this.pos.x, this.pos.y);
     rotate(angle);
-    fill(col,150);
+    noFill();
+    stroke(255);
+    ellipse(0, 0, this.dna[4]);
+    stroke(0, 255, 0);
+    line(0, 0, 0, -this.dna[0] * 25);
+    ellipse(0, 0, 2 * this.dna[2]);
+    stroke(255, 0, 0);
+    line(0, 0, 0, -this.dna[1] * 25);
+    ellipse(0, 0, 2 * this.dna[3]);
+
+    fill(col, 150);
     stroke(col);
     strokeWeight(1);
     beginShape();
@@ -95,5 +125,86 @@ function Rocket() {
     vertex(this.r, this.r * 2);
     endShape(CLOSE);
     pop();
+  };
+
+  this.boundaries = function () {
+    const d = 9; //distance from the edge
+    let trust = 4;
+    let desired = null;
+    //X
+    if (this.pos.x < d) {
+      desired = createVector(trust, this.vel.y);
+    } else if (this.pos.x > width - d) {
+      desired = createVector(-trust, this.vel.y);
+    }
+    //Y
+    if (this.pos.y < d) {
+      desired = createVector(this.vel.x, trust);
+    } else if (this.pos.y > height - d) {
+      desired = createVector(this.vel.x, -trust);
+    }
+
+    if (desired !== null) {
+      //console.log(desired);
+      desired.normalize();
+      desired.mult(trust);
+      const steer = p5.Vector.sub(desired, this.vel);
+      steer.limit(3);
+      this.applyForce(steer);
+    }
+  };
+  //findClosestPartner and applies attraction force
+  this.findClosestPartner = function (list, index) {
+    let d = Infinity;
+    let closest = null; //the closest Rocket
+    //loops through the list, finds the closest
+    for (let k = list.length - 1; k >= 0; k--) {
+      let dn = this.pos.dist(list[k].pos);
+      if ((dn < d) && (dn < this.dna[4]) && (k != index)) {
+        d = dn;
+        closest = list[k];
+      }
+    }
+    return closest;
+  };
+
+  //reproduce 2 individuals
+  this.mate = function (partner) {
+    //console.log(partner.pos);
+    let d = dist(partner.pos.x, partner.pos.y, this.pos.x, this.pos.y);
+    //console.log(d);
+    if (d < 3) {
+      let DNA = [];
+      let p = floor(random(this.dna.length)); //
+
+      //DNA construnction
+      for (let i = 0; i < this.dna.length; i++) {
+        if (i < p) {
+          DNA[i] = this.dna[i]; //mom
+        } else {
+          DNA[i] = partner.dna[i]; //dad
+        }
+      }
+      //console.log(DNA)
+      /* //DNA mutation
+      for (let i = 0; i < dna.length; i++) {
+        if (random() < 0.01) {
+          //1%
+          dna[i] += random(-0.1 * dna[i], 0.1 * dna[i]);
+        }
+      } */
+
+      return new Rocket(createVector(width/2, height/2), DNA);
+    }
+    return null;
+  };
+
+  this.nextGen = function (rocket_list, index) {
+    //console.log(rocket_list);
+    let closest = this.findClosestPartner(rocket_list, index);
+    if (closest != null) {
+      return this.mate(closest);
+    }
+    return null;
   };
 }
--- 02 JsGeneticAlgorithms/07 Steering Vehicles/sketch.js
+++ 02 JsGeneticAlgorithms/07 Steering Vehicles/sketch.js
@@ -1,16 +1,17 @@
 let rockets = [];
 let foods = [];
 let poisons = [];
+let rr = 4;
 
 function setup() {
   createCanvas(400, 400);
   //foods
   for (let i = 0; i < 10; i++) {
-    foods[i] = createVector(random(width), random(height));
+    foods[i] = createVector(random(10,width-10), random(10,height-10));
   }
   //poison
   for (let i = 0; i < 20; i++) {
-    poisons[i] = createVector(random(width), random(height));
+    poisons[i] = createVector(random(10,width), random(10,height));
   }
   //vehicles
   for (let i = 0; i < 3; i++) {
@@ -22,36 +23,45 @@ function draw() {
   background(0);
 
   if (random() < 0.05) {
-    foods.push(createVector(random(width), random(height)));
+    foods.push(createVector(random(10,width-10), random(10,height-10)));
   }
 
-  if (random() < 0.005) {
+  if (random() < 0.0005) {
     rockets.push(new Rocket());
-  }
+  } 
 
   if (random() < 0.01) {
-    poisons.push(createVector(random(width), random(height)));
+    poisons.push(createVector(random(10,width), random(10,height)));
   }
 
   //food
   fill(0, 255, 0);
   for (let i = 0; i < foods.length; i++) {
-    ellipse(foods[i].x, foods[i].y, 20, 20);
+    ellipse(foods[i].x, foods[i].y, 2 * rr);
   }
 
   //poison
   fill(255, 0, 0);
   for (let i = 0; i < poisons.length; i++) {
-    ellipse(poisons[i].x, poisons[i].y, 20, 20);
+    ellipse(poisons[i].x, poisons[i].y, 2 * rr);
   }
 
-  for (let i = rockets.length - 1; i >= 0; i--) {
+  for (let i = 0; i < rockets.length; i++) {
     //console.log(i);
+    rockets[i].boundaries();
     rockets[i].behaviour(foods, poisons);
     rockets[i].update();
     rockets[i].display();
-    rockets[i].health -= 0.01;
+   
+    let child = rockets[i].nextGen(rockets, i)
+    //console.log(child);
+    if (child && rockets.length < 6) {
+      rockets.push(child)
+    } 
+      //rockets.push(rockets[i].mate(nearRocket));
+
   }
+
   for (let i = rockets.length - 1; i >= 0; i--) {
     if (rockets[i].isDeath()) {
       rockets.splice(i, 1);
