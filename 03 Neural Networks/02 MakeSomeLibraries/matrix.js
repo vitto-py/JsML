8f2a21c699b5660321d9fd75b62509d46dc5a25d
Vitto <germanyvitto@gmail.com>
2023-08-16 22:24:29 +0200

    added map

--- 03 Neural Networks/02 MakeSomeLibraries/matrix.js
+++ 03 Neural Networks/02 MakeSomeLibraries/matrix.js
@@ -1,84 +1,96 @@
 // let m = new Matrix(rows, cols)
 
-function Matrix(nrows, ncols) {
-  this.rows = nrows;
-  this.cols = ncols;
-  this.matrix = [];
+class Matrix {
+  constructor(nrows, ncols) {
+    this.rows = nrows;
+    this.cols = ncols;
+    this.matrix = [];
 
-  for (let i = 0; i < this.rows; i++) {
-    this.matrix[i] = [];
-    for (let j = 0; j < this.cols; j++) {
-      this.matrix[i][j] = 0;
+    for (let i = 0; i < this.rows; i++) {
+      this.matrix[i] = [];
+      for (let j = 0; j < this.cols; j++) {
+        this.matrix[i][j] = 0;
+      }
     }
   }
-}
-
-//scalar multiplication
-Matrix.prototype.multiply = function (e) {
-  if (e instanceof Matrix) {
-    //cols = rows 2x5*5x2 = 2x2
-    if (this.cols == e.rows) {
-      let result = new Matrix(this.rows, e.cols);
-      for (let i = 0; i < result.rows; i++) {
-        for (let j = 0; j < result.cols; j++) {
-          //2x2
-          //but I need to loop until 5 this.cols = e.rows
-          let sum = 0;
-          for (let aux = 0; aux < this.cols; aux++) {
-            //go to 5
-            sum += this.matrix[i][aux] * e.matrix[aux][j];
+  //matrix multiplication
+  static multiply(m1, m2) {
+    if (m1 instanceof Matrix && m2 instanceof Matrix) {
+      //cols = rows 2x5*5x2 = 2x2
+      if (m1.cols == m2.rows) {
+        let result = new Matrix(m1.rows, m2.cols);
+        for (let i = 0; i < result.rows; i++) {
+          for (let j = 0; j < result.cols; j++) {
+            //2x2
+            //but I need to loop until 5 m1.cols = m2.rows
+            let sum = 0;
+            for (let aux = 0; aux < m1.cols; aux++) {
+              //go to 5
+              sum += m1.matrix[i][aux] * m2.matrix[aux][j];
+            }
+            //console.log(sum);
+            result.matrix[i][j] = sum;
           }
-          //console.log(sum);
-          result.matrix[i][j] = sum;
         }
+        return result;
+      } else {
+        console.log("ERROR, cols != rows");
+        return undefined;
       }
-      return result;
-    } else {
-      console.log("ERROR, cols != rows");
-      return undefined;
     }
-  } else {
+  }
+  //scalar multiplication
+  multiply(e) {
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
         this.matrix[i][j] *= e;
       }
     }
   }
-};
 
-//add add
-Matrix.prototype.add = function (e) {
-  if (e instanceof Matrix) {
+  //apply func to each element on the matrix
+  map(func) {
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
-        this.matrix[i][j] += e.matrix[i][j];
-      }
-    }
-  } else {
-    //single number
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        this.matrix[i][j] += e;
+        this.matrix[i][j] = func(this.matrix[i][j]);
       }
     }
   }
-};
 
-//initialize random
-Matrix.prototype.randomize = function () {
-  for (let i = 0; i < this.rows; i++) {
-    for (let j = 0; j < this.cols; j++) {
-      this.matrix[i][j] = Math.floor(Math.random() * 10);
+  //add add
+  add(e) {
+    if (e instanceof Matrix) {
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.matrix[i][j] += e.matrix[i][j];
+        }
+      }
+    } else {
+      //single number
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.matrix[i][j] += e;
+        }
+      }
     }
   }
-};
 
-Matrix.prototype.transpose = function () {
-  let m = new Matrix(this.cols, this.rows);
-  for (let i = 0; i < this.rows; i++) {
-    for (let j = 0; j < this.cols; j++) {
-      m.matrix[j][i] = this.matrix[i][j];
+  //initialize random
+  randomize() {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.matrix[i][j] = Math.floor(Math.random() * 10);
+      }
     }
   }
-  return m;
-};
+
+  transpose() {
+    let m = new Matrix(this.cols, this.rows);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        m.matrix[j][i] = this.matrix[i][j];
+      }
+    }
+    return m;
+  }
+}
