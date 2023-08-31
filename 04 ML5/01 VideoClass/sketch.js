e9c4874a467a4bf9132f1154db252275e98ffc9a
Vittorio Fiorito <vitto9311@gmail.com>
2023-08-31 19:06:02 +0200

    Continuous Video Classifier

--- 04 ML5/01 VideoClass/sketch.js
+++ 04 ML5/01 VideoClass/sketch.js
@@ -1,21 +1,24 @@
 let mobileNet;
-let puffin;
+let video;
+let label;
+let prob;
 
 function modelReady() {
   console.log("The model is ready");
-  mobileNet.predict(puffin, gotResult);
+  mobileNet.predict(gotResult);
 }
 function gotResult(error, result) {
   //ERROR first callback
   if (error) {
     console.log(error);
   } else {
-    textSize(32);
-    fill(0, 255, 50);
-    console.log(result)
-    createP(result[0].label);
-    createP(result[0].confidence);
-
+    //console.log(result);
+    label = result[0].label;
+    prob = result[0].confidence;
+    //createP(result[0].label);
+    //createP(result[0].confidence);
+    //Infinity Loop
+    mobileNet.predict(gotResult);
   }
 }
 function imageReady() {
@@ -23,14 +26,20 @@ function imageReady() {
 }
 
 function setup() {
-  createCanvas(200, 200);
-  puffin = createImg("/data/puffin.jpg", imageReady);
+  createCanvas(400, 400);
+  video = createCapture(VIDEO);
   //tiniest model possible
-  puffin.hide();
+  video.hide();
   background(0);
-  mobileNet = ml5.imageClassifier("MobileNet", modelReady);
+  //pass the video as an argument = act continuously
+  mobileNet = ml5.imageClassifier("MobileNet", video, modelReady);
 }
 
 function draw() {
   //background(0);
+  image(video, 0, 0);
+  fill(0);
+  textSize(20);
+  text(label, 10, height - 30);
+  text(prob, 10, height - 10);
 }
